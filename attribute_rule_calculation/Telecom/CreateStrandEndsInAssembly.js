b3078cf5ba4af56ab7d50e13a47a65085ce32a2e
Michael Miller <mmiller@esri.com>
2020-03-27 11:34:08 -0400

    resolve a bug with coordinates

--- attribute_rule_calculation/Telecom/CreateStrandEndsInAssembly.js
+++ attribute_rule_calculation/Telecom/CreateStrandEndsInAssembly.js
@@ -2,16 +2,16 @@
 
 // ***************************************
 // This section has the functions and variables that need to be adjusted based on your implementation
-var assigned_to_field = $feature.terminatorspaceing;
+var assigned_to_field = $feature.terminatorspacing;
 // Instead of assigning the rule at the subtype, it is assigned to all subtypes and returns if not valid
 
 // Limit the rule to valid subtypes
 var valid_asset_groups = [];
 var valid_asset_types = [];
-if (count(valid_asset_groups) > 0 && (valid_asset_groups, $feature.assetgroup) == -1) {
+if (count(valid_asset_groups) > 0 && indexof(valid_asset_groups, $feature.assetgroup) == -1) {
     return assigned_to_field;
 }
-if (count(valid_asset_types) > 0 && (valid_asset_types, $feature.assettype) == -1) {
+if (count(valid_asset_types) > 0 && indexof(valid_asset_types, $feature.assettype) == -1) {
     return valid_asset_types;
 }
 
@@ -20,10 +20,14 @@ var point_count = DefaultValue($feature.terminatorcount, 0);
 var sym_rotation = DefaultValue($feature.symbolrotation, 0);
 var offset_distance = DefaultValue($feature.terminatoroffset, 0);
 
+
 var device_class = 'CommunicationDevice';
 var port_ag = 8;
 var port_at = 144;
 // ************* End Section *****************
+if (point_count <= 0) {
+    return {'ErrorMessage': 'Port count is required'};
+}
 
 var point_geo = Geometry($feature);
 var wkid = point_geo.spatialReference.wkid;
@@ -46,13 +50,13 @@ var new_line = Polyline({"paths": [vertices], "spatialReference": {"wkid": point
 new_line = offset(rotate(new_line, 90 - sym_rotation), offset_distance);
 // Get the first path on the new line
 var first_path = new_line['paths'][0];
-
 // Loop over the path and construct a point at each vertex and return as a strand end
 var new_strand_ends = [];
 for (var i in first_path) {
-    point_x = first_path[i][0];
-    point_y = first_path[i][1];
-    point_z = first_path[i][2];
+
+    point_x = first_path[i].x;
+    point_y = first_path[i].y;
+    point_z = first_path[i].z;
     new_strand_ends[i] = {
         'attributes': {
             'assetgroup': port_ag,
