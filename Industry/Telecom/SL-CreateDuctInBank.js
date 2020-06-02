24accc6aebdcfc7d9a41245f31eb46bde0c0ebee
ted-howard <thoward@esri.com>
2020-06-01 20:15:42 -0600

    added transpostion
    - Properly match from and to port numbers on ducts only when each knockout are the same dimensions and both are empty.
    
    - Added get_port_lookup
      - based on height and width of knockout, gets lookup for what from/to port numbers should be

--- Industry/Telecom/SL-CreateDuctInBank.js
+++ Industry/Telecom/SL-CreateDuctInBank.js
@@ -105,6 +105,30 @@ function next_avail(arr) {
     }
 }
 
+// get lookup for from -> to port numbers
+// example height = 2, width = 3  ->  {"1": 3, "2": 2, "3": 1, "4": 6, "5": 5, "6": 4}
+function get_port_lookup(height, width) {
+    var array = [];
+    var counter = 1;
+    var lookup_dict = {};
+    for (var j = 0; j < height; j++) {
+        var new_row = [];
+        for (var k = 0; k < width; k++) {
+            new_row[k] = counter;
+            counter++;
+        }
+        array[j] = new_row;
+    }
+
+    for (var idx in array) {
+        var rev_row = Reverse(array[idx]);
+        for (var i in array[idx]) {
+            lookup_dict[Text(array[idx][i])] = rev_row[i];
+        }
+    }
+    return lookup_dict;
+}
+
 
 // ************* Validation *****************
 // Limit the rule to valid subtypes
@@ -129,7 +153,9 @@ if (IsEmpty(from_snapped_feat)) {
 }
 // Get count of available duct ports in a knockout. Check using height and width of knockout from attribute fields.
 // Account for ducts that may already be snapped to knockout.
-var from_duct_count = DefaultValue(from_snapped_feat[knock_out_duct_wide_field], 0) * DefaultValue(from_snapped_feat[knock_out_duct_high_field], 0);
+var height_from = DefaultValue(from_snapped_feat[knock_out_duct_high_field], 0);
+var width_from = DefaultValue(from_snapped_feat[knock_out_duct_wide_field], 0);
+var from_duct_count = width_from * height_from;
 var from_duct_occupied = Count(get_snapped_lines(from_point, false));
 if (from_duct_count - from_duct_occupied < duct_count) {
     return {'errorMessage': 'A duct bank has more ducts than the knock out at the start of the line can support.'};
@@ -138,7 +164,9 @@ var to_snapped_feat = get_snapped_point(to_point);
 if (IsEmpty(to_snapped_feat)) {
     return {'errorMessage': 'A duct bank must end at a knock out'};
 }
-var to_duct_count = DefaultValue(to_snapped_feat[knock_out_duct_wide_field], 0) * DefaultValue(to_snapped_feat[knock_out_duct_high_field], 0);
+var height_to = DefaultValue(to_snapped_feat[knock_out_duct_high_field], 0);
+var width_to = DefaultValue(to_snapped_feat[knock_out_duct_wide_field], 0);
+var to_duct_count = width_to * height_to;
 var to_duct_occupied = Count(get_snapped_lines(to_point, false));
 if (to_duct_count - to_duct_occupied < duct_count) {
     return {'errorMessage': 'A duct bank has more ducts than the knock out at the end of the line can support.'};
@@ -146,6 +174,14 @@ if (to_duct_count - to_duct_occupied < duct_count) {
 
 
 // ************* Create Payload *****************
+// special logic if both knockouts are empty and same dimensions H x W
+var port_lookup = null;
+if (from_duct_occupied + to_duct_occupied < 1) {
+    if (height_from == height_to && width_from == width_to){
+        port_lookup = get_port_lookup(height_from, width_from);
+    }
+}
+
 // handle port numbers. used_ports variables are arrays containing integers
 var from_knockout_used_ports = get_used_ports(from_point);
 var to_knockout_used_ports = get_used_ports(to_point);
@@ -164,8 +200,11 @@ for (var j = 0; j < duct_count; j++) {
     if (assign_port_numbers) {
         fromport_value = next_avail(from_knockout_used_ports);
         from_knockout_used_ports[Count(from_knockout_used_ports)] = fromport_value;
-        toport_value = next_avail(to_knockout_used_ports);
-        to_knockout_used_ports[Count(to_knockout_used_ports)] = toport_value
+        if (port_lookup != null) {
+            toport_value = port_lookup[Text(fromport_value)];
+        } else {toport_value = next_avail(to_knockout_used_ports);
+        to_knockout_used_ports[Count(to_knockout_used_ports)] = toport_value;
+        }
     }
     line_attributes = {
         'AssetGroup': duct_AG,
